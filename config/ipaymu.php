2580d82403d5992b29d35489b5d2f26ea69ddecd
lutfi zeindhikra <zeindhikral@gmail.com>
2026-06-17 09:43:08 +0700

    security: pindahkan kredensial iPaymu ke .env

--- config/ipaymu.php
+++ config/ipaymu.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'va' => env('IPAYMU_VA', '1179005282777446'),
-    'api_key' => env('IPAYMU_API_KEY', '8E3DA4EF-1866-4CD0-B9F6-ED7DED6F0D64'),
+    'va' => env('IPAYMU_VA'),
+    'api_key' => env('IPAYMU_API_KEY'),
     'is_production' => env('IPAYMU_IS_PRODUCTION', false),
 ];
